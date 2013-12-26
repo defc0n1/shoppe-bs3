c93c715568d631000f67bc8a1a10ab0c47754a3e
André Tanguy <andre@oceanshq.com>
2013-12-26 23:44:57 +0000

    remove faker and add stripe payment

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -75,36 +75,26 @@ class OrdersController < ApplicationController
   
   def checkout
     @order = Shoppe::Order.find(current_order.id)
-    
-    
     if request.patch?
       @order.attributes = params[:order].permit(:first_name, :last_name, :company, :billing_address1, :billing_address2, :billing_address3, :billing_address4, :billing_country_id, :billing_postcode, :email_address, :phone_number, :delivery_name, :delivery_address1, :delivery_address2, :delivery_address3, :delivery_address4, :delivery_postcode, :delivery_country_id, :separate_delivery_address)
       @order.ip_address = request.ip
       if @order.proceed_to_confirm
         redirect_to checkout_payment_path
       end
-    else
-      # Add some example order data for the example. In a real application
-      # this shouldn't be present.
-      Faker::Config.locale = 'en-gb'
-      @order.first_name = Faker::Name.first_name                                            if @order.first_name.blank?
-      @order.last_name = Faker::Name.last_name                                              if @order.last_name.blank?
-      @order.company = Faker::Company.name                                                  if @order.company.blank?
-      @order.email_address = Faker::Internet.email                                          if @order.email_address.blank?
-      @order.phone_number = Faker::PhoneNumber.phone_number                                 if @order.phone_number.blank?
-      @order.billing_address1 = Faker::Address.building_number + " " + Faker::Address.street_name   if @order.billing_address1.blank?
-      @order.billing_address3 = Faker::Address.city                                                 if @order.billing_address3.blank?
-      @order.billing_address4 = Faker::Address.county                                               if @order.billing_address4.blank?
-      @order.billing_postcode = Faker::Address.zip                                                  if @order.billing_postcode.blank?
     end
   end
-  
+
   def payment
     @order = Shoppe::Order.find(current_order.id)
-    if request.patch?
-      redirect_to checkout_confirmation_path
+    if request.post?
+      if @order.accept_stripe_token(params[:stripe_token])
+        redirect_to checkout_confirmation_path
+      else
+        flash.now[:notice] = "Could not exchange Stripe token. Please try again."
+      end
     end
   end
+
   
   def confirmation
     unless current_order.confirming?
@@ -120,6 +110,10 @@ class OrdersController < ApplicationController
         current_order.payments.create(:method => "Credit Card", :amount => current_order.total, :reference => rand(10000) + 10000, :refundable => true)
         session[:order_id] = nil
         redirect_to root_path, :notice => "Order has been placed!"
+
+        # Fire off to Xero and create invoice (marked as paid)
+        # TODO
+
       rescue Shoppe::Errors::PaymentDeclined => e
         flash[:alert] = "Payment was declined by the bank. #{e.message}"
         redirect_to checkout_path
@@ -128,6 +122,31 @@ class OrdersController < ApplicationController
         redirect_to checkout_path
       end
     end
+  end
+
+  def create_payment_in_xero
+    payment = @client.Payment.build invoice: {id: @invoice.id},
+                                    account: {code: 100},
+                                    date: Date.today,
+                                    amount: @invoice.amount_due,
+                                    reference: "Invoice #{@invoice.id}"
+    payment.save
+  end
+
+  def download_invoice_from_xero
+    invoice = @client.Invoice.find(params[:id])
+    send_data invoice.pdf, type: "application/pdf", filename: "Invoice #{invoice.invoice_number}"
+  end
+
+  def complete_purchase
+    @invoice = @client.Invoice.find(params[:id])
+    create_stripe_customer if !current_registry.has_stripe? and params[:auto_charge]
+    update_customer_credit_card if current_registry.has_stripe? and params[:auto_charge]
+    payment_details = {amount: (@invoice.amount_due * 100).to_i, currency: "gbp", description: "Pay for invoice #{@invoice.invoice_number}"}
+    payment_details.merge! params[:auto_charge] ? { customer: current_registry.stripe_customer_id } : { card: params[:stripe_card_token] }
+
+    @response = Stripe::Charge.create payment_details
+    @xero_response = create_payment_in_xero if @response['paid']
   end
     
 end
